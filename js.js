eaef4b02836199966df4d93797767261b1ae9f5d
pabloborrajo1988 <45261360+pabloborrajo1988@users.noreply.github.com>
2018-12-21 10:29:00 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,4 @@
+var a=[[1,2],[3,4],[5,6]];
+alert("longitud de la lista:"+a.length);
+alert("longitud del primer elemento de la lista:"+a[0].length);
+alert("valoe del tercer elemento en la posicion segunda:"+a[2][1]);
